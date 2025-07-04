1691737454ca926b37094f12ff02511c42b05896
Dan Russo <rdaniele669@gmail.com>
2025-07-04 10:25:32 +0200

    persistence to db and first class/table User

---
+++ src/main/java/danielerusso/Application.java
@@ -0,0 +1,13 @@
+package danielerusso;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4w3progsett");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/danielerusso/entities/User.java
@@ -0,0 +1,75 @@
+package danielerusso.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long user_id;
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+    @Column(name = "card_number")
+    private long cardNo;
+
+    public User() {
+    }
+
+    public User(String name, String surname, LocalDate birthday, long cardNo) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.cardNo = cardNo;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public long getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(long cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthday=" + birthday +
+                ", cardNo=" + cardNo +
+                '}';
+    }
+}
